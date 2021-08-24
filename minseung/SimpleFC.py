7388b3bb188c96bfd9f1a5fa47dddc632beac119
leeminseung <mkms3580@gmail.com>
2021-08-24 21:10:00 +0900

    add skeleton module

---
+++ minseung/SimpleFC.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import os
+
+class SimpleFC(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, device, lr=0.0005):
+        super().__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size*4),
+            nn.ReLU(),
+            nn.Linear(hidden_size*4, hidden_size*2),
+            nn.ReLU(),
+            nn.Linear(hidden_size*2, output_size),
+        )
+
+        self.device = device
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+
+    def forward(self, x):
+        output = self.encoder(x)
+        return output
+
+    def get_latent(self, x):
+        return self.encoder(x)
+
+    def save_model(self, path):
+        torch.save(self.state_dict(), os.path.join("model_loss", path, "model.pt"))
+
+    def load_model(self, path):
+        self.load_state_dict(torch.load(os.path.join("model_loss", path, "model.pt")))
---
+++ minseung/train_model.py
@@ -0,0 +1,115 @@
+import torch.nn as nn
+import numpy as np
+import torch
+import os
+import tqdm
+import matplotlib.pyplot as plt
+import datetime
+import argparse
+from get_logger import get_logger
+from get_dataloader import get_dataloader
+from preprocess import get_ski_data
+from SimpleFC import SimpleFC
+
+def train_model(model, start_time, dataset, args, logger, device):
+    loss_fn = nn.MSELoss()
+    epochs = args.epochs
+    train_losses = []
+    val_losses = []
+    best_val_loss = 1e8
+
+    train_dataloader, val_dataloader, test_dataloader = get_dataloader(dataset, device, args)
+
+    for epoch in tqdm.tqdm(range(epochs)):
+        loss_sum = 0
+        for state_and_action, next_state in train_dataloader:
+            if len(state_and_action.shape) > 2:
+                state_and_action = state_and_action.reshape(state_and_action.shape[0], -1)
+            state_and_action = state_and_action.to(device)
+            predicted_next_state = model(state_and_action)
+            loss = loss_fn(predicted_next_state, next_state.to(device))
+
+            loss.backward()
+            model.optimizer.step()
+            model.optimizer.zero_grad()
+
+            loss_sum += loss.item()
+        
+        train_losses.append(loss_sum/len(train_dataloader))
+        msg = '{} Epoch, Train Mean Loss: {}'.format(epoch, loss_sum/len(train_dataloader))
+        logger.info(msg)
+
+        loss_sum = 0
+        with torch.no_grad():
+            for state_and_action, next_state in val_dataloader:
+                if len(state_and_action.shape) > 2:
+                    state_and_action = state_and_action.reshape(state_and_action.shape[0], -1)
+                state_and_action = state_and_action.to(device)
+                predicted_next_state = model(state_and_action)
+                loss = loss_fn(predicted_next_state, next_state.to(device))
+
+                loss_sum += loss.item()
+        
+        val_losses.append(loss_sum/len(val_dataloader))
+        msg = '{} Epoch, Validation Mean Loss: {}'.format(epoch, loss_sum/len(val_dataloader))
+        logger.info(msg)
+        
+        plt.plot(train_losses, label='train loss', color='r')
+        plt.plot(val_losses, label='validation loss', color='b')
+
+        if epoch == 0:
+            plt.legend()
+        plt.savefig(os.path.join('model_loss', start_time, start_time + '.png'))
+        np.save(os.path.join('model_loss', start_time, 'train_losses.npy'), np.array(train_losses))
+        np.save(os.path.join('model_loss', start_time, 'val_losses.npy'), np.array(val_losses))
+
+        if loss_sum < best_val_loss:
+            model.save_model(start_time)
+            best_val_loss = loss_sum
+            msg = '\n\n\t Best Model Saved!!! \n'
+            logger.info(msg)
+
+    return model
+    
+if __name__ == '__main__':
+    # get Arguments
+    parser = argparse.ArgumentParser(description='SKI: Traning Model Args')
+    parser.add_argument('--epochs', default=300, type=int, help='Set epochs to train Model')
+    parser.add_argument('--batch_size', default=3000, type=int, help='Batch size used in dataloader')
+    parser.add_argument('--num_workers', default=32, type=int, help='Num workers used in dataloader')
+    parser.add_argument('--input_size', default=54, type=int, help='Number of hidden nodes')
+    parser.add_argument('--hidden_node', default=64, type=int, help='Number of hidden nodes')
+
+    args = parser.parse_args()
+
+    start_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    
+    # make directory
+    if not os.path.exists("model_loss"):
+        os.mkdir("model_loss")
+
+    if os.path.isdir(os.path.join("model_loss", start_time)) and os.path.exists(os.path.join("model_loss", start_time)):
+        print('Already Existing Directory. Please wait for 1 minute.')
+        exit()
+
+    os.mkdir(os.path.join("model_loss", start_time))
+
+    # Make and initialize model
+    model = SimpleFC(input_size=args.input_size, hidden_size=args.hidden_node, output_size=5, device=device)
+    model.to(device)
+
+    # get data
+    dataset = np.load("/home/ms/ski_from_scratch/minseung/step_size_3, sequential_length_10.npy")
+
+    # record model structure
+    system_logger = get_logger(name='Autoencoder model', file_path=os.path.join('model_loss', start_time, start_time + '_train_log.log'))
+
+    system_logger.info('===== Arguments information =====')
+    system_logger.info(vars(args))
+
+    system_logger.info('===== Model Structure =====')
+    system_logger.info(model)
+
+    system_logger.info('===== Loss History of Transition Model =====')
+    model = train_model(model, start_time, dataset, args, system_logger, device)
